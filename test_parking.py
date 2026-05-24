e09928cd0a688a58e4138cf40ae4df10568af372
Araksab <baskarayudha02@gmail.com>
2026-05-25 05:56:24 +0700

    testing parking
    test: add failing test for public

--- test_parking.py
+++ test_parking.py
@@ -1,5 +1,4 @@
 from parking import calculate_parking_fee
 
 def test_public_holiday_flat_rate():
-    # Menguji aturan: Public holiday flat rate = $15 for all vehicles
     assert calculate_parking_fee('car', 5, 'weekday', True) == 15
